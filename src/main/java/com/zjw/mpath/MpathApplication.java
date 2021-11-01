9790a99d3bc3de0ee286e5b1b23a0fe222934b34
jingweihaha <jingweizhangcs@gmail.com>
2021-11-01 10:32:53 -0400

    init commit

---
+++ src/main/java/com/zjw/mpath/MpathApplication.java
@@ -0,0 +1,13 @@
+package com.zjw.mpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpathApplication.class, args);
+    }
+
+}
